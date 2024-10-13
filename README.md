6d423defa5186bc1557fec0b2a607eb92be1a60b
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-10-14 02:09:20 +0300

    Improve

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Symfony Property Access is incompatible with PHP 8.4
 
+Run:
+
 ```shell
 composer install
 php index.php
 ```
+
+See output:
+
+```txt
+array(2) {
+  ["property"] => string(8) "readonly"
+  ["PropertyAccessor::isWritable()"] => bool(false)
+}
+array(3) {
+  ["property"] => string(18) "public_private_set"
+  ["PropertyAccessor::isWritable()"] => bool(true)
+  ["PropertyAccessor::setValue()"] => string(127) "Fail! Cannot modify private(set) property Foo::$public_private_set from scope Symfony\Component\PropertyAccess\PropertyAccessor"
+}
+array(3) {
+  ["property"] => string(20) "public_protected_set"
+  ["PropertyAccessor::isWritable()"] => bool(true)
+  ["PropertyAccessor::setValue()"] => string(131) "Fail! Cannot modify protected(set) property Foo::$public_protected_set from scope Symfony\Component\PropertyAccess\PropertyAccessor"
+}
+array(3) {
+  ["property"] => string(19) "virtual_no_set_hook"
+  ["PropertyAccessor::isWritable()"] => bool(true)
+  ["PropertyAccessor::setValue()"] => string(53) "Fail! Property Foo::$virtual_no_set_hook is read-only"
+}
+```
+
+Expected `["PropertyAccessor::isWritable()"] => bool(false)` for all properties.
--- index.php
+++ index.php
@@ -4,18 +4,43 @@ use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-final class User
+$accessor = new PropertyAccessor();
+
+final class Foo
 {
-    public function __construct(
-        public private(set) string $name = 'Bob',
-   ) {}
+    public readonly bool $readonly;
+    public private(set) bool $public_private_set;
+    public protected(set) bool $public_protected_set;
+    public bool $virtual_no_set_hook { get => true; }
 }
 
-$accessor = new PropertyAccessor();
-$user = (new ReflectionClass(User::class))->newInstanceWithoutConstructor();
+$object = new Foo();
 
-// bool(true)
-var_dump($accessor->isWritable($user, 'name'));
+foreach (new ReflectionClass(Foo::class)->getProperties() as $property) {
+    $writable = $accessor->isWritable($object, $property->name);
 
-// Uncaught Error: Cannot modify private(set) property User::$name from scope Symfony\Component\PropertyAccess\PropertyAccessor
-$accessor->setValue($user, 'name', 'Jack');
+    if (!$writable) {
+        var_dump([
+            'property' => $property->name,
+            'PropertyAccessor::isWritable()' => false,
+        ]);
+
+        continue;
+    }
+
+    try {
+        $accessor->setValue($object, $property->name, false);
+
+        var_dump([
+            'property' => $property->name,
+            'PropertyAccessor::isWritable()' => true,
+            'PropertyAccessor::setValue()' => 'OK!',
+        ]);
+    } catch (Throwable $exception) {
+        var_dump([
+            'property' => $property->name,
+            'PropertyAccessor::isWritable()' => true,
+            'PropertyAccessor::setValue()' => 'Fail! '.$exception->getMessage(),
+        ]);
+    }
+}
